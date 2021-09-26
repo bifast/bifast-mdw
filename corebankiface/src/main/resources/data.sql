aa9311a1cb17739f3722a025345b3ae7df5f639d
yogi45 <Yogi.Lestari@mii.co.id>
2021-09-26 13:11:24 +0700

    data Account

---
+++ corebankiface/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO cb_account VALUES (1, '7723004444', 'CACC', 'Satu dua tiga', 200000.00, '7723004444', 'CACC', '123455555', 'ANDI ANDI', 'Resident', 'ACCEPTED', 'Jakarta', 'Private', '040502345');
+INSERT INTO cb_account VALUES (2, '7723002222', 'CACC', 'Satu dua tiga', 200000.00, '7723004444', 'CACC', '123455555', 'Diana', 'Resident', 'HOLD', 'Jakarta', 'Private', '023232323');
