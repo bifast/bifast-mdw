f088575a3dcd39a9e6a053478f33a91542c17a01
Frans Mazhar <frans.mazhar@mii.co.id>
2022-03-08 10:08:21 +0700

    R-1.2

--- bifast-lib/src/main/java/bifast/library/adapter/GregorianCalendarXMLAdapter.java
+++ bifast-lib/src/main/java/bifast/library/adapter/GregorianCalendarXMLAdapter.java
@@ -1,6 +1,5 @@
 package bifast.library.adapter;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
 
@@ -8,22 +7,19 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class GregorianCalendarXMLAdapter extends XmlAdapter<String, XMLGregorianCalendar> {
-	private static Logger logger = LoggerFactory.getLogger(GregorianCalendarXMLAdapter.class);
+//	private static Logger logger = LoggerFactory.getLogger(GregorianCalendarXMLAdapter.class);
 
 	@Override
 	public XMLGregorianCalendar unmarshal(String v) throws Exception {
 		GregorianCalendar cal = new GregorianCalendar();
-		if (v.length() == 19)
+		if (v.trim().length() == 19)
 			cal.setTime(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(v));
-		else if (v.length() == 20)
+		else if (v.trim().length() == 20)
 			cal.setTime(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").parse(v));
-		else if (v.length() == 23)
+		else if (v.trim().length() == 23)
 			cal.setTime(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS").parse(v));
-		else if (v.length() == 24)
+		else if (v.trim().length() == 24)
 			cal.setTime(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(v));
 		
         XMLGregorianCalendar calendar = DatatypeFactory.newInstance().newXMLGregorianCalendar( cal);
