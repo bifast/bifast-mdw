ce002b1fdcfa758c781dbd1e878d1426ffeeb1fd
Fransmzh1 <frans.mazhar@gmail.com>
2022-07-12 10:05:45 +0700

    DELETE komi-inbound dan komi-mock

--- bifast-mock/src/main/java/bifast/mock/persist/InboundCounterRepository.java
+++
@@ -1,9 +0,0 @@
-package bifast.mock.persist;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InboundCounterRepository extends JpaRepository<InboundCounter, Integer> {
-
-}
